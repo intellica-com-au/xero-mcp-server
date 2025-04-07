3b90ebf05a634895d200e27c122d9dd12320d53b
Stanislav-Pankrashin <stasera2@gmail.com>
2025-04-08 11:38:33 +1200

    broken auth provider

--- src/helpers/create-xero-tool.ts
+++ src/helpers/create-xero-tool.ts
@@ -1,6 +1,48 @@
 import { ZodRawShape } from "zod";
 import { ToolCallback } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { ToolDefinition } from "../types/tool-definition.js";
+import { xeroClient } from "../clients/xero-client.js";
+import { formatError } from "./format-error.js";
+import { auth } from "@modelcontextprotocol/sdk/client/auth.js";
+import ListInvoicesTool from "../tools/list/list-invoices.tool.js";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const authMiddleware = <Args extends undefined | ZodRawShape = any>(
+//   callback: ToolCallback<Args>,
+// ): (...args) => {
+//   return async (args) => {
+//     try {
+//       await xeroClient.authenticate();
+
+//       return await callback(args);
+//     } catch (error) {
+//       return {
+//         result: null,
+//         isError: true,
+//         error: formatError(error),
+//       };
+//     }
+//   };
+// };
+
+const authMiddleware = (handler: ToolCallback<any>): ToolCallback<any> => {
+  return async (args, args2) => {
+    try {
+      await xeroClient.authenticate();
+
+      return await handler(args, args2);
+    } catch (error) {
+      return {
+        content: [
+          {
+            type: "text" as const,
+            text: `Error: ${formatError(error)}`,
+          },
+        ],
+      };
+    }
+  };
+};
 
 export const CreateXeroTool =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,5 +56,5 @@ export const CreateXeroTool =
       name: name,
       description: description,
       schema: schema,
-      handler: handler,
+      handler: authMiddleware(handler),
     });
