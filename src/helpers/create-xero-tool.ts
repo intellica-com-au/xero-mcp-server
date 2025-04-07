3e7e6d82b33f1390cf0db8b170d03cd8c8472692
Stanislav-Pankrashin <stasera2@gmail.com>
2025-04-08 11:44:52 +1200

    unhook it for now

--- src/helpers/create-xero-tool.ts
+++ src/helpers/create-xero-tool.ts
@@ -1,10 +1,10 @@
 import { ZodRawShape } from "zod";
 import { ToolCallback } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { ToolDefinition } from "../types/tool-definition.js";
-import { xeroClient } from "../clients/xero-client.js";
-import { formatError } from "./format-error.js";
-import { auth } from "@modelcontextprotocol/sdk/client/auth.js";
-import ListInvoicesTool from "../tools/list/list-invoices.tool.js";
+// import { xeroClient } from "../clients/xero-client.js";
+// import { formatError } from "./format-error.js";
+// import { auth } from "@modelcontextprotocol/sdk/client/auth.js";
+// import ListInvoicesTool from "../tools/list/list-invoices.tool.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 // const authMiddleware = <Args extends undefined | ZodRawShape = any>(
@@ -25,24 +25,24 @@ import ListInvoicesTool from "../tools/list/list-invoices.tool.js";
 //   };
 // };
 
-const authMiddleware = (handler: ToolCallback<any>): ToolCallback<any> => {
-  return async (args, args2) => {
-    try {
-      await xeroClient.authenticate();
+// const authMiddleware = (handler: ToolCallback<any>): ToolCallback<any> => {
+//   return async (args, args2) => {
+//     try {
+//       await xeroClient.authenticate();
 
-      return await handler(args, args2);
-    } catch (error) {
-      return {
-        content: [
-          {
-            type: "text" as const,
-            text: `Error: ${formatError(error)}`,
-          },
-        ],
-      };
-    }
-  };
-};
+//       return await handler(args, args2);
+//     } catch (error) {
+//       return {
+//         content: [
+//           {
+//             type: "text" as const,
+//             text: `Error: ${formatError(error)}`,
+//           },
+//         ],
+//       };
+//     }
+//   };
+// };
 
 export const CreateXeroTool =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -56,5 +56,5 @@ export const CreateXeroTool =
       name: name,
       description: description,
       schema: schema,
-      handler: authMiddleware(handler),
+      handler: handler,
     });
