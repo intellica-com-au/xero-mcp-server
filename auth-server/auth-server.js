76583033e40ed600c112768f81d8539d7041da0b
Stanislav-Pankrashin <stasera2@gmail.com>
2025-04-08 11:02:10 +1200

    memory store

--- auth-server/auth-server.js
+++ auth-server/auth-server.js
@@ -11,45 +11,44 @@ const redirect_uri = `http://localhost:${port}/callback`;
 const scopes =
   "openid profile email accounting.transactions accounting.contacts accounting.settings.read";
 
-var codeVerifier = "";
-var accessToken = "";
+const memoryStore = {
+  codeVerifier: "",
+  accessToken: "",
+};
 
 // TODO change this
 app.get("/callback", async (req, res) => {
   const { code } = req.query;
 
-  // const response = await axios.post(
-  //   "https://identity.xero.com/connect/token",
-  //   {
-  //     grant_type: "authorization_code",
-  //     client_id: clientId,
-  //     code: code,
-  //     redirect_uri,
-  //     code_verifier: codeVerifier,
-  //   },
-  //   {
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //       Accept: "application/json",
-  //     },
-  //   },
-  // );
+  const response = await axios.post(
+    "https://identity.xero.com/connect/token",
+    {
+      grant_type: "authorization_code",
+      client_id: clientId,
+      code: code,
+      redirect_uri,
+      code_verifier: memoryStore.codeVerifier,
+    },
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+    },
+  );
 
-  // accessToken = response.data.access_token;
-  // console.log("accessToken", accessToken);
+  memoryStore.accessToken = response.data.access_token;
 
   res.sendFile(path.join(path.resolve(), "/auth-server/"));
 });
 
 app.get("/login-url", (req, res) => {
-  codeVerifier = `${crypto.randomUUID()}-${crypto.randomUUID()}`;
+  memoryStore.codeVerifier = `${crypto.randomUUID()}-${crypto.randomUUID()}`;
   const codeChallenge = createHash("sha256")
-    .update(codeVerifier, "ascii")
+    .update(memoryStore.codeVerifier, "ascii")
     .digest("base64url");
 
   const url = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirect_uri}&scope=${scopes}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
-
-  console.log("codeVerifier", codeVerifier);
 
   res.send({ url });
 });
